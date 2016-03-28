c7dc9dfc3a2eda9fab3b5047eceec00af7d63361
teseo <sol@javiermellado.com>
2016-03-28 23:09:29 +0100

    data persitst

--- mysql56/run.sh
+++ mysql56/run.sh
@@ -3,22 +3,32 @@
 VOLUME_HOME="/var/lib/mysql"
 
 # Tweaks to give Apache/PHP write permissions to the app
-chown -R mysql:staff /var/lib/mysql
-chown -R mysql:staff /var/run/mysqld
-chmod -R 770 /var/lib/mysql
-chmod -R 770 /var/run/mysqld
+#chown -R mysql:staff /var/lib/mysql
+#chown -R mysql:staff /var/run/mysqld
+#chmod -R 770 /var/lib/mysql
+#chmod -R 770 /var/run/mysqld
 
-if [ -n "$VAGRANT_OSX_MODE" ];then
-    usermod -u $DOCKER_USER_ID mysql
-    groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
-    groupmod -g ${DOCKER_USER_GID} staff
-fi
+#if [ -n "$VAGRANT_OSX_MODE" ];then
+ #   usermod -u $DOCKER_USER_ID mysql
+  #  groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
+   # groupmod -g ${DOCKER_USER_GID} staff
+#fi
 
 # Tweaks to give MySQL write permissions to the app
-chmod -R 770 /var/lib/mysql
-chmod -R 770 /var/run/mysqld
-chown -R mysql:staff /var/lib/mysql
-chown -R mysql:staff /var/run/mysqld
+#chmod -R 770 /var/lib/mysql
+#chmod -R 770 /var/run/mysqld
+#chown -R mysql:staff /var/lib/mysql
+#chown -R mysql:staff /var/run/mysqld
+
+TARGET_UID=$(stat -c "%u" /var/lib/mysql)
+echo '-- Setting mysql user to use uid '$TARGET_UID
+usermod -o -u $TARGET_UID mysql || true
+
+TARGET_GID=$(stat -c "%g" /var/lib/mysql)
+echo '-- Setting mysql group to use gid '$TARGET_GID
+groupmod -o -g $TARGET_GID mysql || true
+
+chown -R mysql:root /var/run/mysqld/
 
 sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 sed -i "s/user.*/user = mysql/" /etc/mysql/my.cnf
