1e9b147cc96d1b083846bfa78399e30492185371
Javier Mellado <sol@javiermellado>
2016-03-29 00:07:48 +0100

    last commit

--- mysql56/run.sh
+++ mysql56/run.sh
@@ -2,24 +2,6 @@
 
 VOLUME_HOME="/var/lib/mysql"
 
-# Tweaks to give Apache/PHP write permissions to the app
-#chown -R mysql:staff /var/lib/mysql
-#chown -R mysql:staff /var/run/mysqld
-#chmod -R 770 /var/lib/mysql
-#chmod -R 770 /var/run/mysqld
-
-#if [ -n "$VAGRANT_OSX_MODE" ];then
- #   usermod -u $DOCKER_USER_ID mysql
-  #  groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
-   # groupmod -g ${DOCKER_USER_GID} staff
-#fi
-
-# Tweaks to give MySQL write permissions to the app
-#chmod -R 770 /var/lib/mysql
-#chmod -R 770 /var/run/mysqld
-#chown -R mysql:staff /var/lib/mysql
-#chown -R mysql:staff /var/run/mysqld
-
 TARGET_UID=$(stat -c "%u" /var/lib/mysql)
 echo '-- Setting mysql user to use uid '$TARGET_UID
 usermod -o -u $TARGET_UID mysql || true
